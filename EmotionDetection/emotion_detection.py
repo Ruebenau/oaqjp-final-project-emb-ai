240c49ee969a664aa8b88d8e6fc31f69222491fd
Aaron Nau <aaronnau2002@gmail.com>
2025-11-18 13:28:16 -0500

    Task 6&7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,7 +6,20 @@ def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict';
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"};
     input = { "raw_document": { "text": text_to_analyse }};
-    response = json.loads(requests.post(url, json=input, headers=header).text);
+    full_response = requests.post(url, json=input, headers=header)
+    response = json.loads(full_response.text);
+
+    status_code = full_response.status_code
+
+    if(status_code == 400):
+        return {   
+                        "anger": None, 
+                        "disgust": None, 
+                        "fear": None, 
+                        "joy": None, 
+                        "sadness": None, 
+                        "dominant emotion": None
+                    }
 
     emotion_scores = response["emotionPredictions"][0]["emotion"]
 
---
+++ server.py
@@ -0,0 +1,34 @@
+"""
+This is the main server module for my Flask application.
+"""
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask(__name__)
+
+@app.route("/")
+def starting_page():
+    """
+    This function renders the landing page of my application.
+    """
+    return render_template("index.html")
+
+@app.route("/emotionDetector")
+def call_emotion_detector():
+    """
+    This function calls the emotion detector.
+    """
+    text = request.args.get("textToAnalyze")
+    result = emotion_detector(text)
+
+    if result["dominant emotion"] is None:
+        return "Invalid text! Please try again!"
+
+    anger = result["anger"]
+    disgust = result["disgust"]
+    fear = result["fear"]
+    joy = result["joy"]
+    sadness = result["sadness"]
+    dominant_emotion = result["dominant emotion"]
+
+    return f"For the given statement, the system response is \'anger\': {anger}, \'disgust\': {disgust}, \'fear\': {fear}, \'joy\': {joy} and \'sadness\': {sadness}. The dominant emotion is {dominant_emotion}"
